b917c2b730f1d2b07a0934501702ff98a9360d15
yobicod <yobicod.4u@gmail.com>
2024-04-27 09:33:23 +0700

    init project go-101

---
+++ hello.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"html"
+)
+
+func main() {
+	fmt.Println("Hello")
+	fmt.Println("Go-101")
+	fmt.Print("Hello")
+	fmt.Print("Go-101")
+
+	fmt.Println(true && false)
+
+	fmt.Println(`Test new
+	krub`)
+
+	const htmlCode = "<p>Hello Go-101<p/>"
+	en := html.EscapeString(htmlCode)
+	de := html.UnescapeString(htmlCode)
+	fmt.Println(en)
+	fmt.Println(de)
+
+}
